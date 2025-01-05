dfa27027cc18d7207e6b012e8ee42a826db48ae5
Eisai <12467320+Eisaichen@users.noreply.github.com>
2025-01-05 01:03:44 -0600

    Update NVIDIA Driver 566.36

--- build.ps1
+++ build.ps1
@@ -3,7 +3,6 @@ $url = $($i | Where-Object {$_.tag_name -eq "$env:GH_CI_TAG"}).assets.browser_do
 [string]$7z="$PWD"+'\build'
 
 
-docker pull mcr.microsoft.com/windows/server:ltsc2022
 .\wget -q --no-hsts $url -O .\ComfyUI_windows_portable_nvidia.7z
 Invoke-Expression ".\7z\7za.exe x .\ComfyUI_windows_portable_nvidia.7z -o$7z"
 Rename-Item -Path .\build\ComfyUI_windows_portable\ComfyUI\models -NewName _models
@@ -13,9 +12,9 @@ $i=Get-Content .\build\ComfyUI_windows_portable\README_VERY_IMPORTANT.txt
 Set-Content .\build\ComfyUI_windows_portable\README_VERY_IMPORTANT.txt $i[14..$i.length]
 
 if ($env:GH_CI_LATEST -eq "true") {
-    docker build --isolation hyperv --no-cache -t eisai/comfy-ui:latest -t eisai/comfy-ui:$env:GH_CI_TAG .\build
+    docker build --isolation hyperv --pull --no-cache -t eisai/comfy-ui:latest -t eisai/comfy-ui:$env:GH_CI_TAG .\build
 } else {
-    docker build --isolation hyperv --no-cache -t eisai/comfy-ui:$env:GH_CI_TAG .\build
+    docker build --isolation hyperv --pull --no-cache -t eisai/comfy-ui:$env:GH_CI_TAG .\build
 }
 
 if ($env:GH_CI_PUSH -eq "true") {
